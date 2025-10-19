b167e035678dc5f85df29fd72a686e6b6103a6b6
SunshineBee36 <ignacio29101998@gmail.com>
2025-10-18 21:02:12 -0300

    Actualizado CalculateBaseDamage para aplicar la division fisico especial por movimientos

--- src/pokemon.c
+++ src/pokemon.c
@@ -2452,7 +2452,7 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
         if (attackerHoldEffect == sHoldEffectToType[i][0]
             && type == sHoldEffectToType[i][1])
         {
-            if (IS_TYPE_PHYSICAL(type))
+            if (gBattleMoves[move].category == MOVE_CATEGORY_PHYSICAL)
                 attack = (attack * (attackerHoldEffectParam + 100)) / 100;
             else
                 spAttack = (spAttack * (attackerHoldEffectParam + 100)) / 100;
@@ -2506,7 +2506,7 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
     if (gBattleMoves[gCurrentMove].effect == EFFECT_EXPLOSION)
         defense /= 2;
 
-    if (IS_TYPE_PHYSICAL(type))
+    if (gBattleMoves[move].category == MOVE_CATEGORY_PHYSICAL)
     {
         if (gCritMultiplier == 2)
         {
@@ -2561,7 +2561,7 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
     if (type == TYPE_MYSTERY)
         damage = 0; // is ??? type. does 0 damage.
 
-    if (IS_TYPE_SPECIAL(type))
+    else if (gBattleMoves[move].category == MOVE_CATEGORY_SPECIAL)
     {
         if (gCritMultiplier == 2)
         {
